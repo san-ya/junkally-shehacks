72ae5fe45dc937f4db94815a9a3d4a7a17a4de4b
san-ya <83218056+san-ya@users.noreply.github.com>
2022-03-12 22:37:19 +0530

    modifying app.js to contain roots

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 // @ROUTES
 const home = require("./routes/home");
+const register = require("./routes/register");
+const login = require("./routes/login");
 
 const app = express();
 
@@ -14,11 +16,24 @@ app.use(express.static(__dirname + "/public"));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
 // *get routes
 app.get("/", home.root);
+app.get("/how-we-work", home.work);
+app.get("/login", login.loginGET)
+app.get("/register", register.registerGET)
+app.post("/login", login.loginPOST)
+app.post("/register", register.registerPOST)
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("Server started at port 3000");
